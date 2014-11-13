51137fe89db70211018b6f61946eb3ad581b6870
Chris-Bishop <christopher.allan.bishop@gmail.com>
2014-11-13 12:32:54 -0500

    Added drop selection for the price

--- cheesus/app/models/restaurant.rb
+++ cheesus/app/models/restaurant.rb
@@ -1,2 +1,19 @@
 class Restaurant < ActiveRecord::Base
+# has_many :prices
+
+# def price_range
+# 	"#{lowesr} - #{highest}"
+#   end
+# class Price < ActiveRecord::Base
+#   belongs_to :restaurant
+# end
+
+
+
+# # collection_select(:price, :restaurant, Restaurant.all, :price_range, prompt: true)
+
+
+
+
+
 end
