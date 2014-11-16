9bb35c5fac7f3525fec87da158646a6a6034a654
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2014-11-16 12:17:36 -0500

    Added bootstrap

--- cheesus/app/models/r_user.rb
+++ cheesus/app/models/r_user.rb
@@ -4,5 +4,11 @@ class RUser < ActiveRecord::Base
 	has_one :restaurant
 	has_many :reservations, through: :restaurants
 
+	# validate :validate_num_restaurants
 
+	# def validate_num_restaurants
+	# 	if @r_user.restaurants > 1
+	# 		errors.add(:restaurant, "can't have more than 1")
+	# 	end
+	# end
 end
