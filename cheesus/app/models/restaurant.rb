55260fe5a0e6840aa560d297989066a5c4570e2a
wduntak <wduntak@gmail.com>
2014-11-16 15:08:18 -0500

    Added css styling to buttons and headers

--- cheesus/app/models/restaurant.rb
+++ cheesus/app/models/restaurant.rb
@@ -4,6 +4,16 @@ class Restaurant < ActiveRecord::Base
 	has_many :reservations
 	has_many :reviews
 
+	# validate :more_than_one, on: :create
+
+	# def more_than_one
+	# 	@user = r_current_user
+	# 	if @user.my_restaurant != nil
+	# 		errors.add("You can only have one restaurant.")
+	# 	end
+	# end
+
+
 	def available(party_size, date)
 		reserved = reservations.where(date: date).sum(party_size)
 		capacity - (reserved + party_size) >= 0
